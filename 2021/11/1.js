e47f03a509c27853f1b77d31f9149d7595e0674d
Simon Berggren <simon.berggren@geshdo.com>
2021-12-12 20:26:10 +0100

    Day 11

---
+++ 2021/11/1.js
@@ -0,0 +1,58 @@
+const fs = require('fs');
+const path = require('path');
+
+const inputPath = path.resolve(__dirname, 'input.txt');
+const input = fs.readFileSync(inputPath).toString();
+
+const grid = input.split('\n').map(row => row.split('').map(Number));
+
+const flashPositions = [];
+
+const doFlash = (x, y) => {
+    if (x >= 0 && x < 10 && y >= 0 && y < 10) {
+        if (++grid[y][x] == 10) {
+            flashPositions.push({ x, y });
+        }
+    }
+}
+
+const flashNearby = ({ x, y }) => {
+    doFlash(x - 1, y);
+    doFlash(x + 1, y);
+    doFlash(x, y - 1);
+    doFlash(x, y + 1);
+    doFlash(x + 1, y + 1);
+    doFlash(x - 1, y - 1);
+    doFlash(x - 1, y + 1);
+    doFlash(x + 1, y - 1);
+}
+
+let count = 0;
+
+for (let i = 0; i < 100; i++) {
+
+    for (const y in grid) {
+        const row = grid[y];
+        for (const x in row) {
+            if (++row[x] == 10) {
+                flashPositions.push({ x: Number(x), y: Number(y) });
+            }
+        }
+    }
+
+    while (flashPositions.length) {
+        flashNearby(flashPositions.shift());
+    }
+
+    for (const y in grid) {
+        const row = grid[y];
+        for (const x in row) {
+            if (row[x] > 9) {
+                row[x] = 0;
+                ++count;
+            }
+        }
+    }
+}
+
+console.log(count);
