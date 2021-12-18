1da20b70642c98489eb4176c49af0c6beccd5971
Simon Berggren <simon.berggren@geshdo.com>
2021-12-18 20:12:02 +0100

    Day 17

---
+++ 2021/17/2.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+const path = require('path');
+
+const inputPath = path.resolve(__dirname, 'input.txt');
+const input = fs.readFileSync(inputPath).toString();
+
+const widthIndex = input.search(/-?[0-9]+..-?[0-9]+/);
+
+const [widthString] = /-?[0-9]+..-?[0-9]+/.exec(input);
+const heightString = /-?[0-9]+..-?[0-9]+/.exec(input.slice(widthIndex + widthString.length))[0];
+
+const [startX, endX] = widthString.split('..').map(Number);
+const [startY, endY] = heightString.split('..').map(Number);
+
+const isInGoal = ([x, y]) => x >= startX && x <= endX && y >= startY && y <= endY;
+
+let count = 0;
+
+for (let startXVel = 1; startXVel < 1000; startXVel++) {
+    for (let startYVel = -1000; startYVel < 1000; startYVel++) {
+
+        let xVel = startXVel;
+        let yVel = startYVel;
+        let currentPos = [0, 0];
+
+        while (xVel != 0 || currentPos[1] > endY) {
+
+            let [x, y] = currentPos;
+            currentPos = [x + xVel, y + yVel];
+            if (isInGoal(currentPos)) {
+                ++count;
+                break;
+            }
+
+            if (xVel < 0) {
+                xVel += 1;
+            } else if (xVel > 0) {
+                xVel -= 1;
+            }
+
+            yVel -= 1;
+        }
+
+    }
+}
+console.log(count);
